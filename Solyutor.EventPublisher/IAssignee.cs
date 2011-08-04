fb6fa37deffa8e539561562fbc39fdf5f3c209f6
solyutor <yurysold@googlemail.com>
2011-08-04 22:28:48 +0400

    cleaned up IAssignee.cs

--- Solyutor.EventPublisher/IAssignee.cs
+++ Solyutor.EventPublisher/IAssignee.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Solyutor.EventPublisher
+﻿namespace Solyutor.EventPublisher
 {
     public interface IAssignee
     {
