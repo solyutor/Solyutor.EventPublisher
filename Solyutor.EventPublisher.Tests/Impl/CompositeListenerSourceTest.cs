b42db113eafc798fd66e9aff7c1f808523144d52
j.soldatenkov <yurysold@googlemail.com>
2011-08-06 13:57:57 +0300

    Implemented CompositeListenerSource

---
+++ Solyutor.EventPublisher.Tests/Impl/CompositeListenerSourceTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using SharpTestsEx;
+using Solyutor.EventPublisher.Impl;
+
+namespace Solyutor.EventPublisher.Tests.Impl
+{
+    [TestFixture]
+    public class CompositeListenerSourceTest
+    {
+        [Test]
+        public void Resolve_listners_from_all_sources()
+        {
+            var compositeSource = new CompositeListenerSource();
+
+            var firstListener = new TestListener();
+            var secondListener = new TestListener();
+
+            var firstSource = new SimpleAssignee();
+            var secondSource = new SimpleAssignee();
+
+            firstSource.Subscribe(firstListener);
+            secondSource.Subscribe(secondListener);
+
+            compositeSource.AddSource(firstSource);
+            compositeSource.AddSource(secondSource);
+
+            var listeners = new List<IListener<TestMessage>>(compositeSource.ResolveListenersFor<TestMessage>());
+
+            listeners.Satisfy(list =>
+                              list.Contains(firstListener) &&
+                              list.Contains(secondListener));
+        }
+    }
+}
---
+++ Solyutor.EventPublisher/Impl/CompositeListenerSource.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace Solyutor.EventPublisher.Impl
+{
+    public class CompositeListenerSource : IListenerSource
+    {
+        private readonly ISet<IListenerSource> _sources;
+
+        public CompositeListenerSource()
+        {
+            _sources = new HashSet<IListenerSource>();
+        }
+
+        #region IListenerSource Members
+
+        public IEnumerable<IListener<TMessage>> ResolveListenersFor<TMessage>()
+        {
+            var result = new HashSet<IListener<TMessage>>();
+            foreach (var listenerSource in _sources)
+            {
+                foreach (var listener in listenerSource.ResolveListenersFor<TMessage>())
+                {
+                    result.Add(listener);
+                }
+            }
+            return result;
+        }
+
+        #endregion
+
+        public void AddSource(IListenerSource listenerSource)
+        {
+            _sources.Add(listenerSource);
+        }
+    }
+}
