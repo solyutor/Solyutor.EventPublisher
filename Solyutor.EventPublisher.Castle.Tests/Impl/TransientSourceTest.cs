3d83a10d0a3fe97f2048fd812e647581bab3348b
j.soldatenkov <yurysold@googlemail.com>
2011-08-06 14:50:00 +0300

    Implemented TransientSource

---
+++ Solyutor.EventPublisher.Castle.Tests/Impl/TransientSourceTest.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+using NUnit.Framework;
+using Solyutor.EventPublisher.Castle.Impl;
+
+namespace Solyutor.EventPublisher.Castle.Tests.Impl
+{
+    [TestFixture]
+    public class TransientSourceTest
+    {
+        [Test]
+        public void Resolve_returns_transient_instances_from_kernel()
+        {
+            var windsor = new WindsorContainer();
+            windsor.Register(
+                Component.For<ITransientListener<Message>>()
+                    .ImplementedBy<TestTransientListener>());
+            
+            var source = new TransientSource(windsor.Kernel);
+
+            var listenters = source.ResolveListenersFor<Message>();
+
+            Assert.That(listenters.OfType<ITransientListener<Message>>().Count(), Is.EqualTo(1));
+
+        }
+    }
+
+    public class Message
+    {
+    }
+
+    public class TestTransientListener : ITransientListener<Message>
+    {
+        public Message Message;
+
+        #region ITransientListener<Message> Members
+
+        public void ListenTo(Message message)
+        {
+            Message = message;
+        }
+
+        #endregion
+    }
+}
---
+++ Solyutor.EventPublisher.Castle/Impl/TransientSource.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Castle.MicroKernel;
+using Solyutor.EventPublisher.Impl;
+
+namespace Solyutor.EventPublisher.Castle.Impl
+{
+    public class TransientSource : IListenerSource
+    {
+        private readonly IKernel _kernel;
+
+        public TransientSource(IKernel kernel)
+        {
+            _kernel = kernel;
+        }
+
+        public virtual IEnumerable<IListener<TMessage>> ResolveListenersFor<TMessage>()
+        {
+            return _kernel.ResolveAll<ITransientListener<TMessage>>();
+        }
+    }
+}
