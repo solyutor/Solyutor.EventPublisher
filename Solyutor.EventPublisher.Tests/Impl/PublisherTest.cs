1ef39951c11ac35d75bdcc5342e8dcf7fa833548
j.soldatenkov <yurysold@googlemail.com>
2011-08-06 13:41:33 +0300

    Implemented publisher

---
+++ Solyutor.EventPublisher.Tests/Impl/PublisherTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using Solyutor.EventPublisher.Impl;
+
+namespace Solyutor.EventPublisher.Tests.Impl
+{
+    [TestFixture]
+    public class PublisherTest
+    {
+        [Test]
+        public void Publish_will_send_the_message_to_listner()
+        {
+            var assignee = new SimpleAssignee();
+            var publishWay = new SimplePublishWay();
+            var listener = new TestListener();
+            var message = new TestMessage();
+            var publisher = new Publisher(assignee, publishWay);
+
+            assignee.Subscribe(listener);
+
+            publisher.Publish(message);
+
+            Assert.That(listener.TestMessage, Is.SameAs(message));
+        }
+    }
+}
---
+++ Solyutor.EventPublisher/Impl/Publisher.cs
@@ -0,0 +1,22 @@
+﻿namespace Solyutor.EventPublisher.Impl
+{
+    public class Publisher : IPublisher
+    {
+        private readonly IListenerSource _listenerSource;
+        private readonly IPublishWay _publishWay;
+
+        public Publisher(IListenerSource listenerSource, IPublishWay publishWay)
+        {
+            _listenerSource = listenerSource;
+            _publishWay = publishWay;
+        }
+
+        public void Publish<TMessage>(TMessage message)
+        {
+            foreach (var listener in _listenerSource.ResolveListenersFor<TMessage>())
+            {
+                _publishWay.Publish(message, listener);
+            }
+        }
+    }
+}
