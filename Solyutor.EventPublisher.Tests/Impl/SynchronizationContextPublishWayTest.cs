05000a797c51c46e8c4334f279ce4ca4b32cfe0a
j.soldatenkov <yurysold@googlemail.com>
2011-08-07 13:43:14 +0300

    Added SyncronizationContextPublish

---
+++ Solyutor.EventPublisher.Tests/Impl/SynchronizationContextPublishWayTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading;
+using NUnit.Framework;
+using Solyutor.EventPublisher.Impl;
+
+namespace Solyutor.EventPublisher.Tests.Impl
+{
+    [TestFixture]
+    public class SynchronizationContextPublishWayTest
+    {
+        [Test]
+        public void Publish_will_call_listner()
+        {
+            var listener = new TestListener();
+            var message = new TestMessage();
+
+            var publishWay = new SynchronizationContextPublishWay(new SynchronizationContext());
+
+            publishWay.Publish(message, listener);
+
+            Thread.Sleep(10); //Wait for async call
+
+            Assert.That(listener.TestMessage, Is.SameAs(message));
+        }
+    }
+}
---
+++ Solyutor.EventPublisher/Impl/SyncronizationContextPublishWay.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading;
+
+namespace Solyutor.EventPublisher.Impl
+{
+    public class SynchronizationContextPublishWay : IPublishWay
+    {
+        private readonly SynchronizationContext _synchronizationContext;
+
+        public SynchronizationContextPublishWay(SynchronizationContext synchronizationContext)
+        {
+            _synchronizationContext = synchronizationContext;
+        }
+
+        public void Publish<TMessage>(TMessage message, IListener<TMessage> listener)
+        {
+            _synchronizationContext.Post(x => listener.ListenTo(message), null);
+        }
+    }
+}
