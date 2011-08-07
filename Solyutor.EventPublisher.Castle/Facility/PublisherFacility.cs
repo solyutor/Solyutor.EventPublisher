e9767b18cc1f59a18721cd03346522e16b350233
j.soldatenkov <yurysold@googlemail.com>
2011-08-07 12:35:10 +0300

    Refactoring of PublisherFacility

--- Solyutor.EventPublisher.Castle/Facility/PublisherFacility.cs
+++ Solyutor.EventPublisher.Castle/Facility/PublisherFacility.cs
@@ -10,7 +10,6 @@ namespace Solyutor.EventPublisher.Castle.Facility
     public class PublisherFacility : AbstractFacility
     {
         private readonly IPublishWay _publishWay;
-        private IAssignee _assignee;
 
         public PublisherFacility()
         {
@@ -23,7 +22,7 @@ namespace Solyutor.EventPublisher.Castle.Facility
 
         protected override void Init()
         {
-            _assignee = ResolveOfCreateAssignee();
+            ResolveOfCreateAssignee();
 
             Kernel.Register(
                 Component.For<IPublisher>().UsingFactoryMethod(CreatePublisher));
@@ -31,43 +30,25 @@ namespace Solyutor.EventPublisher.Castle.Facility
 
         private IPublishWay ResolvePublishWay()
         {
-            IPublishWay result = null;
-            if (Kernel.HasComponent(typeof (IPublishWay)))
-            {
-                result = Kernel.Resolve<IPublishWay>();
-            }
-            else
-            {
-                result = _publishWay;
-            }
+            IPublishWay result = Kernel.HasComponent(typeof (IPublishWay)) ? Kernel.Resolve<IPublishWay>() : _publishWay;
 
             if (result == null)
                 throw new InvalidOperationException(
-                    "Publisher facility needs an instance of IPublishWay. Supply it using PublishFacility constructor or register it in advance in the container.");
+                    "Publisher facility needs an instance of IPublishWay. Supply it using PublishFacility constructor or register it with container.");
 
             return result;
         }
 
-        private IAssignee ResolveOfCreateAssignee()
+        private void ResolveOfCreateAssignee()
         {
-            IAssignee assignee = null;
-
-            if (Kernel.HasComponent(typeof (IAssignee)))
-            {
-                assignee = Kernel.Resolve<IAssignee>();
-            }
-            else
-            {
-                assignee = new SimpleAssignee();
-                Kernel.Register(
-                    Component.For<IAssignee, IListenerSource>().Instance(assignee));
-            }
-            return assignee;
+            if (Kernel.HasComponent(typeof (IAssignee))) return;
+            Kernel.Register(
+                Component.For<IAssignee, IListenerSource>().Instance(new SimpleAssignee()));
         }
 
         public IPublisher CreatePublisher()
         {
-            IPublishWay publishWay = ResolvePublishWay();
+            var publishWay = ResolvePublishWay();
 
             var sources = new List<IListenerSource>(Kernel.ResolveAll<IListenerSource>()) {new TransientSource(Kernel)};
 
