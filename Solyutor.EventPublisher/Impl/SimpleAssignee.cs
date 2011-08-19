6ab8054ef7465595e46079dc85fc450ccc924c1d
Jury Soldatenkov <yurysold@googlemail.com>
2011-08-19 04:26:12 +0400

    SimpleAssignee was made thread-safe

--- Solyutor.EventPublisher/Impl/SimpleAssignee.cs
+++ Solyutor.EventPublisher/Impl/SimpleAssignee.cs
@@ -6,25 +6,36 @@ namespace Solyutor.EventPublisher.Impl
     public class SimpleAssignee : IAssignee
     {
         private readonly ISet<object> _listeners;
+        private readonly object _latch;
 
         public SimpleAssignee()
         {
             _listeners = new HashSet<object>();
+            _latch = new object();
         }
 
         public virtual void Subscribe(object listener)
         { 
-            _listeners.Add(listener);
+            lock(_latch)
+            {
+                _listeners.Add(listener);
+            }
         }
 
         public virtual void Unsubscribe(object listener)
         {
-            _listeners.Remove(listener);
+            lock(_latch)
+            {
+                _listeners.Remove(listener);
+            )
         }
 
         public virtual IEnumerable<IListener<TMessage>> ResolveListenersFor<TMessage>()
         {
-            return _listeners.OfType<IListener<TMessage>>();
+            lock(_latch)
+            {
+                return _listeners.OfType<IListener<TMessage>>().ToList();
+            }
         }
     }
 }
