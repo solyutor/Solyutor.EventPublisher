76cc941a0c76f0daf05776f7229a887e8fa96bb2
j.soldatenkov <yurysold@googlemail.com>
2011-10-01 13:50:17 +0300

    Implemented registration helper for Autofac

---
+++ Solyutor.EventPublisher.Autofac.Tests/Module/PerDependencyListenersTest.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using Autofac;
+using NUnit.Framework;
+using Solyutor.EventPublisher.Autofac.Module;
+using Solyutor.EventPublisher.Tests.Impl;
+
+namespace Solyutor.EventPublisher.Autofac.Tests.Module
+{
+    [TestFixture]
+    public class PerDependencyListenersTest
+    {
+        private ContainerBuilder _builder;
+        private IContainer _container;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _builder = new ContainerBuilder();
+
+            _builder.RegisterPerDependencyListenersFrom(Assembly.GetExecutingAssembly());
+
+            _container = _builder.Build();
+        }
+
+        [Test]
+        public void Register_per_dependency_instances()
+        {
+            _container.Resolve<IListener<TestMessage>>();
+        }
+
+        [Test]
+        public void Return_different_instances_when_resoleved()
+        {
+            var first = _container.Resolve<IListener<TestMessage>>();
+
+            var second = _container.Resolve<IListener<TestMessage>>();
+
+            Assert.That(first, Is.Not.SameAs(second));
+        }
+    }
+}
---
+++ Solyutor.EventPublisher.Autofac/Module/PerDependencyListeners.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Reflection;
+using Autofac;
+
+namespace Solyutor.EventPublisher.Autofac.Module
+{
+    public static class PerDependencyListeners
+    {
+         public static void RegisterPerDependencyListenersFrom(this ContainerBuilder builder, params Assembly[] assemblies)
+         {
+             builder.RegisterAssemblyTypes(assemblies)
+                 .Where(type => type.IsPerDependencyListener())
+                 .AsClosedTypesOf(typeof(IListener<>))
+                 .InstancePerDependency();
+         }
+
+         private static bool IsPerDependencyListener(this Type self)
+        {
+            var interfaces = self.FindInterfaces(
+                (@interface, nomatter) => @interface.IsGenericType &&
+                                          @interface.GetGenericTypeDefinition() == typeof (IPerDependencyListener<>),
+                null);
+
+            return interfaces.Length > 0;
+        }
+    }
+}
