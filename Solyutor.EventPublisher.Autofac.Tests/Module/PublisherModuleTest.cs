6b4ed104a64a4420739013a8d54eff1da684d2f9
j.soldatenkov <yurysold@googlemail.com>
2011-10-01 11:05:45 +0300

    Added tests to PublisherModuleTest

--- Solyutor.EventPublisher.Autofac.Tests/Module/PublisherModuleTest.cs
+++ Solyutor.EventPublisher.Autofac.Tests/Module/PublisherModuleTest.cs
@@ -18,6 +18,14 @@ namespace Solyutor.EventPublisher.Autofac.Tests.Module
         }
 
         [Test]
+        public void Register_publisher()
+        {
+            var container = BuildContainer();
+
+            var publisher = container.Resolve<IPublisher>();
+        }
+
+        [Test]
         [ExpectedException(typeof(ArgumentNullException))]
         public void Module_throws_if_no_publishway_were_supplied()
         {
