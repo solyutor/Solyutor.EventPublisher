4c7d8b01554abdbe3624c4206fdcecdc83db42b6
j.soldatenkov <yurysold@googlemail.com>
2011-10-01 14:08:15 +0300

    Added test for AutofacCompositeListnersSource

---
+++ Solyutor.EventPublisher.Autofac.Tests/Module/AutofacCompositeListenerSourceSource.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using System.Reflection;
+using Autofac;
+using NUnit.Framework;
+using SharpTestsEx;
+using Solyutor.EventPublisher.Autofac.Module;
+using Solyutor.EventPublisher.Impl;
+using Solyutor.EventPublisher.Tests.Impl;
+
+namespace Solyutor.EventPublisher.Autofac.Tests.Module
+{
+    [TestFixture]
+    public class AutofacCompositeListenerSourceSource : IListener<TestMessage>
+    {
+        [Test]
+        public void Can_get_listeners_from_all_sources()
+        {
+            var builder = new ContainerBuilder();
+        
+            builder.RegisterModule(new PublisherModule(new SimplePublishWay()));
+            builder.RegisterPerDependencyListenersFrom(Assembly.GetExecutingAssembly());
+
+            var container = builder.Build();
+
+            var assigne = container.Resolve<IAssignee>();
+            assigne.Subscribe(this);
+
+            var listenerSource = container.Resolve<IListenerSource>();
+
+            
+            var listeners = listenerSource.ResolveListenersFor<TestMessage>();
+
+
+            listeners.Satisfy(l =>
+                l.Contains(this) &&
+                l.Any( x => x.GetType() == typeof(TestPerDependencyListener)));
+        }
+
+        public void ListenTo(TestMessage message)
+        {
+            
+        }
+    }
+}
