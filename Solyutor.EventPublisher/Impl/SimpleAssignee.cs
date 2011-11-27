fb15ef3a9068b20cb90674346cec42d626f4d088
j.soldatenkov <yurysold@googlemail.com>
2011-11-28 00:05:14 +0400

    SimpleAssignee will use dictionary to improve perfomance

--- Solyutor.EventPublisher/Impl/SimpleAssignee.cs
+++ Solyutor.EventPublisher/Impl/SimpleAssignee.cs
@@ -1,41 +1,80 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Solyutor.EventPublisher.Impl
 {
     public class SimpleAssignee : IAssignee
     {
-        private readonly ISet<object> _handlers;
         private readonly object _latch;
+        private readonly Dictionary<Type, ISet<object>> _storeHandlers;
+        private readonly HashSet<object> _emptySet;
 
         public SimpleAssignee()
         {
-            _handlers = new HashSet<object>();
+            _storeHandlers = new Dictionary<Type, ISet<object>>();
+            _emptySet = new HashSet<object>();
             _latch = new object();
         }
 
         public virtual void Subscribe(object handler)
-        { 
+        {
+            var interfaces = GetImplementedHandlers(handler);
+
+            if(interfaces.Length == 0) return;
+  
             lock(_latch)
             {
-                _handlers.Add(handler);
+                foreach (var @interface in interfaces)
+                {
+                    ISet<object> handlers;
+                    if(_storeHandlers.TryGetValue(@interface, out handlers))
+                    {
+                        handlers.Add(handler);
+                    }
+                    else
+                    {
+                        _storeHandlers[@interface] = new HashSet<object> { handler };
+                    }
+                }
             }
         }
 
         public virtual void Unsubscribe(object handler)
         {
+            var interfaces = GetImplementedHandlers(handler);
+            if (interfaces.Length == 0) return;
+
             lock (_latch)
             {
-                _handlers.Remove(handler);
+                foreach (var @interface in interfaces)
+                {
+                    ISet<object> handlers;
+                    if (_storeHandlers.TryGetValue(@interface, out handlers))
+                    {
+                        handlers.Remove(handler);
+                    }
+                }
             }
         }
 
         public virtual IEnumerable<IHandler<TMessage>> ResolveHandlersFor<TMessage>()
         {
+            ISet<object> handlers;
             lock(_latch)
             {
-                return _handlers.OfType<IHandler<TMessage>>().ToList();
+                _storeHandlers.TryGetValue(typeof (IHandler<TMessage>), out handlers);
+                handlers = new HashSet<object>(handlers ?? _emptySet);
             }
+            return handlers.Cast<IHandler<TMessage>>();
+        }
+
+        private static Type[] GetImplementedHandlers(object handler)
+        {
+            var interfaces = handler.GetType().FindInterfaces(
+                (@interface, nomatter) => @interface.IsGenericType &&
+                                          @interface.GetGenericTypeDefinition() == typeof (IHandler<>), null);
+            return interfaces;
         }
     }
 }
